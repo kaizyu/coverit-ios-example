500e98ef5b584f7f80a0714c19fbcfe286fc2f5b
Infitini <infiniti@coverit.io>
2015-05-10 20:31:37 +0800

    Bootstrap

---
+++ CoveritExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoveritExample
+//
+//  Created by Infiniti on 5/10/15.
+//  Copyright (c) 2015 coverit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
